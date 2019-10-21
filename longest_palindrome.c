5022d5bd8cb5d5ee5cc781fb903c5f1826681a2b
raghus100 <raghus100@gmail.com>
2019-10-21 14:44:40 -0700

    Longest palindrome fixed logic issue

--- longest_palindrome.c
+++ longest_palindrome.c
@@ -6,20 +6,24 @@ char * longestPalindrome(char * s)
     int res[1000][1000] = {{0}};
     int len = strlen(s);
     int res_i = 0, res_j = 0;
+    int i, j;
     char *palindrome = NULL;
     /*Str of len 1 is palindrome*/
-    for(int i = 0; i < len; i++)
-       res[i][i] = 0; 
+    for(i = 0; i < len; i++)
+       res[i][i] = 1; 
 
     /*Find if string of len 2 is palindrome*/
-    for(int i = 0; i < len-1; i++) {
-        if(s[i] == s[i+1])
+    for(i = 0; i < len-1; i++) {
+        if(s[i] == s[i+1]) {
             res[i][i+1] = 2;
+            res_j = i+1;
+            res_i = i;
+        }
     }
     
     /*Find if string of len > 2 is palindrome*/
-    for(int i = 0; i < len; i++){
-        for(int j = i+2; j < len-2; j++) {
+    for(i = 0; i < len-2; i++){
+        for(j = i+2; j < len; j++) {
             if((res[i+1][j-1] > 0) && s[i] == s[j]) {
                 res[i][j] = j-i+1;
                 if((res_j-res_i+1) < (j-i+1)) {
@@ -27,6 +31,13 @@ char * longestPalindrome(char * s)
                 }
             }
         } 
+    }
+
+    for(i = 0; i < len; i++) {
+        for(j = 0; j < len; j++) {
+            printf("%d ", res[i][j]);
+        }
+        printf("\n");
     }
     palindrome = calloc(1, res_j-res_i+1);
     strncpy(palindrome, s+res_i, res_j-res_i+1);
@@ -36,7 +47,7 @@ char * longestPalindrome(char * s)
 int main()
 {
     char *res = NULL;
-    char *s = "badab";
+    char *s = "cbbd";
     printf("Longest palindrome: %s\n", longestPalindrome(s));
     return 0;
 }
